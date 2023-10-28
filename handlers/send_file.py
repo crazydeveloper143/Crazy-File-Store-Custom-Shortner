9f675cd278018a1f3746740396f43969bf154b3e
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-10-27 21:47:15 -0700

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -5,38 +5,85 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
+#strram
+from urllib.parse import quote_plus
+from util.file_properties import get_name, get_hash, get_media_file_size
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"**𝙁𝙞𝙡𝙚𝙨 𝙬𝙞𝙡𝙡 𝙗𝙚 𝘿𝙚𝙡𝙚𝙩𝙚𝙙 𝘼𝙛𝙩𝙚𝙧 10 𝙢𝙞𝙣 ⏰**\n",
+            f"**Files will be Deleted After 30 min ⏰**\n",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
         await reply_forward(message, file_id)
-        
+
+
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                lazy_file = await bot.copy_message(chat_id=STREAM_LOGS, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id)
+
+
+                lazy_stream = f"{STREAM_URL}watch/{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+                lazy_download = f"{STREAM_URL}{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+                
+                fileName = quote_plus(get_name(lazy_file))
+
+                await lazy_file.reply_text(
+                    text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+                    quote=True,
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=lazy_download),  # we download Link
+                                                        InlineKeyboardButton('▶Stream online', url=lazy_stream)]])  # web stream Link
+                )
+                return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                          message_id=file_id, 
+                                          reply_markup=InlineKeyboardMarkup(
+                                            [
+                                                [
+                                                  InlineKeyboardButton("Fast Download", url=lazy_download),
+                                                  InlineKeyboardButton("▶Stream online", url=lazy_stream),
+                                                ],
+                                            ]),
+                                            )
         elif Config.FORWARD_AS_COPY is False:
-            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            lazy_file = await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
+            lazy_stream = f"{STREAM_URL}watch/{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+            lazy_download = f"{STREAM_URL}{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+            fileName = quote_plus(get_name(lazy_file))
+            await lazy_file.reply_text(
+                text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+                quote=True,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=lazy_download),  # we download Link
+                                                    InlineKeyboardButton('▶Stream online', url=lazy_stream)]])  # web stream Link
+            )
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id,
+                                              reply_markup=InlineKeyboardMarkup(
+                                            [
+                                                [
+                                                  InlineKeyboardButton("Fast Download", url=lazy_download),
+                                                  InlineKeyboardButton("▶Stream online", url=lazy_stream),
+                                                ],
+                                            ]),
+                                            )
+
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
-    
-async def delete_file(file_id: int):
-    await asyncio.sleep(600)  # wait for 30 minutes
-    # Delete the file using the file ID
-    # Code to delete the file goes here
-  
 
-    await sent_message.delete()  # delete the file using Pyrogram
-    
+
+async def delete_file(file_id: int):
+    await asyncio.sleep(1800)  # wait for 30 minutes
+
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
     asyncio.create_task(delete_file(file_id))  # schedule the file deletion task
     await asyncio.sleep(2)
-
