0fc838d01824e87999e24f8239b599e3bb6c9474
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-07-04 20:27:38 -0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,26 +1,24 @@
-# # (c) @AbirHasan2005
-
 import os
 from os import getenv, environ
 
-STREAM_URL = os.environ.get("STREAM_URL", "https://crazybotz-shivam31635.koyeb.app/")
+STREAM_URL = os.environ.get("STREAM_URL", "https://shivam.koyeb.app/")
 STREAM_LOGS = environ.get('STREAM_LOGS','-1001890815456')
 
 class Config(object):
 	API_ID = int(os.environ.get("API_ID", "29499299"))
 	API_HASH = os.environ.get("API_HASH", "d2a224d0d84bf24b6b82e887a4f1559a")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5911870660:AAEr0HeSms5_4FauHPb1BjYh1krgRTTOy4Q")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "File_Store4_Bot")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6530848860:AAH3HpuUmuofZR9HjQlOWP0gjkGUM9RUZUA")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "MFileStorebot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001799438122"))
 	BOT_OWNER = [int(owner) for owner in os.environ.get("BOT_OWNER", "6249148586").split()]
-	DATABASE_URL = os.environ.get('DATABASE_URI', "mongodb+srv://filestortebot:filestortebot@cluster0.tdmgwvj.mongodb.net/?retryWrites=true&w=majority")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	DATABASE_URL = os.environ.get('DATABASE_URI', "mongodb+srv://Animestorebot:Animestorebot@cluster0.kzwrcpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001600537706")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", -1001799438122)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
 	
 	ABOUT_BOT_TEXT = f"""<b>
 ╭────[ *𝙁𝙞𝙡𝙚 𝙎𝙩𝙤𝙧𝙚 𝘽𝙤𝙩*]────⍟
