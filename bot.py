268e7d18dd73c8f21e89f2441bcba191be59cca7
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-07-05 04:23:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,8 +63,9 @@ async def start(bot: Client, cmd: Message):
     if cmd.from_user.id in Config.BANNED_USERS:
         await cmd.reply_text("Sorry, You are banned.")
         return
-        
-    usr_cmd = cmd.text.split("_", 1)[-1] > 1 else "/start"
+
+    usr_cmd = cmd.text.split("_", 1)[-1] if "_" in cmd.text else "/start"
+
     try:
         user = await bot.get_chat_member(Config.UPDATES_CHANNEL, cmd.from_user.id)
     except UserNotParticipant:
@@ -83,6 +84,7 @@ async def start(bot: Client, cmd: Message):
     except ChatAdminRequired:
         await cmd.reply_text("Bot needs to be an admin in the channel to check membership status.")
         return
+
     if usr_cmd == "/start":
         await add_user_to_database(bot, cmd)
         await cmd.reply_text(
@@ -106,8 +108,10 @@ async def start(bot: Client, cmd: Message):
                 file_id = int(b64_to_str(usr_cmd).split("_")[-1])
             except (Error, UnicodeDecodeError):
                 file_id = int(usr_cmd.split("_")[-1])
+
             GetMessage = await bot.get_messages(chat_id=Config.DB_CHANNEL, message_ids=file_id)
             message_ids = []
+
             if GetMessage.text:
                 message_ids = GetMessage.text.split(" ")
                 _response_msg = await cmd.reply_text(
@@ -117,8 +121,10 @@ async def start(bot: Client, cmd: Message):
                 )
             else:
                 message_ids.append(int(GetMessage.id))
+
             for i in range(len(message_ids)):
                 await send_media_and_reply(bot, user_id=cmd.from_user.id, file_id=int(message_ids[i]))
+
         except Exception as err:
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
 
