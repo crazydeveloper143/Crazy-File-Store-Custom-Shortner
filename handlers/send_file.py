20fcb60e9962c80155f4c52d838c5d47b595f9e5
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-07-10 01:30:16 -0700

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -5,37 +5,61 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
-async def reply_forward(message: Message, file_id: int):
-    try:
-        await message.reply_text(
-            f"**𝙁𝙞𝙡𝙚𝙨 𝙬𝙞𝙡𝙡 𝙗𝙚 𝘿𝙚𝙡𝙚𝙩𝙚𝙙 𝘼𝙛𝙩𝙚𝙧 10 𝙢𝙞𝙣 ⏰**\n",
-            disable_web_page_preview=True, quote=True)
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        await reply_forward(message, file_id)
+#strram
+from urllib.parse import quote_plus
+from util.file_properties import get_name, get_hash, get_media_file_size
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
         
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
-        if Config.FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id)
-        elif Config.FORWARD_AS_COPY is False:
-            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                              message_ids=file_id)
+        markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("🔻 ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ 🔻", callback_data=f"stream#{file_id}")
+                ],
+            ]
+        )
+
+        if Config.FORWARD_AS_COPY:
+            return await bot.copy_message(
+                chat_id=user_id,
+                from_chat_id=Config.DB_CHANNEL,
+                message_id=file_id,
+                reply_markup=markup
+            )
+        else:
+            return await bot.forward_messages(
+                chat_id=user_id,
+                from_chat_id=Config.DB_CHANNEL,
+                message_ids=file_id,
+                reply_markup=markup
+            )
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return media_forward(bot, user_id, file_id)
-    
-async def delete_file(file_id: int):
-    await asyncio.sleep(600)  # wait for 30 minutes
-    # Delete the file using the file ID
-    # Code to delete the file goes here
-  
+        return await media_forward(bot, user_id, file_id)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    await sent_message.delete()  # delete the file using Pyrogram
-    
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
-    sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(message=sent_message, file_id=file_id)
-    asyncio.create_task(delete_file(file_id))  # schedule the file deletion task
-    await asyncio.sleep(2)
+    await media_forward(bot, user_id, file_id)
+
+@Bot.on_callback_query(filters.regex(r"^stream#"))
+async def stream_download(bot, query):
+    file_id = query.data.split('#', 1)[1] 
+    lazy_file = await media_forward(bot, Config.STREAM_LOGS, file_id)
+    file_name = quote_plus(get_name(lazy_file))
+    lazy_stream = f"{Config.STREAM_URL}watch/{str(lazy_file.id)}/{file_name}?hash={get_hash(lazy_file)}"
+    lazy_download = f"{Config.STREAM_URL}{str(lazy_file.id)}/{file_name}?hash={get_hash(lazy_file)}"
+    await query.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("📥 ᴅᴏᴡɴʟᴏᴀᴅ 📥", url=lazy_stream),
+                    InlineKeyboardButton("🖥️ ꜱᴛʀᴇᴇᴍ 🖥️", url=lazy_download)
+                ],
+                [
+                    InlineKeyboardButton('⁉️ ᴄʟᴏsᴇ ⁉️', callback_data='close_data')
+                ]
+            ]
+        )
+    )
