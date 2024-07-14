1147cf6fea3ab2b88594a49539ca91db94e6c648
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-07-14 01:51:01 -0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -15,8 +15,8 @@ class Config(object):
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", -1002223038346)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	FORWARD_AS_COPY = os.environ.get("FORWARD_AS_COPY", "False").lower() in ("true", "1", "t", "y", "yes")
-	BROADCAST_AS_COPY = os.environ.get("BROADCAST_AS_COPY", "False").lower() in ("true", "1", "t", "y", "yes")
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	
