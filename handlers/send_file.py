13856f7b5311e7973a9119921ff0c746080a52d8
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-07-17 08:14:33 -0700

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -4,6 +4,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
+from handlers.save_media import get_file_caption
 
 #strram
 from urllib.parse import quote_plus
@@ -20,23 +21,33 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             ]
         )
 
+        caption = await get_file_caption(file_id)
         if Config.FORWARD_AS_COPY:
-            return await bot.copy_message(
+            sent_message = await bot.copy_message(
                 chat_id=user_id,
                 from_chat_id=Config.DB_CHANNEL,
                 message_id=file_id,
                 reply_markup=markup
             )
         else:
-            return await bot.forward_messages(
+            sent_message = await bot.forward_messages(
                 chat_id=user_id,
                 from_chat_id=Config.DB_CHANNEL,
                 message_ids=file_id,
                 reply_markup=markup
             )
+
+        if caption:
+            await bot.edit_message_caption(
+                chat_id=sent_message.chat.id,
+                message_id=sent_message.message_id,
+                caption=caption,
+                reply_markup=markup
+            )
+
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return media_forward(bot, user_id, file_id)
+        return await media_forward(bot, user_id, file_id)
     except Exception as e:
         print(f"An error occurred: {e}")
 
